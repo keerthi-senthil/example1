a3c7fb10312776d417e15ce8a925037bd501e620
Keerthana S <keerthisenthil1161@gmail.com>
2021-10-18 20:20:44 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,2 @@
-<script type="text/javascript">  
-document.write("JavaScript is a simple language for javatpoint learners");  
-</script>  
+console.log('This example is different!');
+console.log('The result is displayed in the Command Line Interface');
